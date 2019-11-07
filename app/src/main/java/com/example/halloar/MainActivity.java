14b84fcc773e1a7ecc13d8eff861b1f910f2d6d8
DennisGadomski <Dennis.Gadomski@STUDENT.PXL.BE>
2019-11-07 08:52:05 +0100

    Added line and cylinder anchor (Mike)

--- app/src/main/java/com/example/halloar/MainActivity.java
+++ app/src/main/java/com/example/halloar/MainActivity.java
@@ -19,7 +19,9 @@ import com.google.ar.core.Frame;
 import com.google.ar.core.Pose;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.MaterialFactory;
@@ -72,7 +74,6 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
         tvHeight = findViewById(R.id.tvHeight);
         tvWidth = findViewById(R.id.tvWidth);
         tvLength = findViewById(R.id.tvLength);
-
         initModel();
 
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
@@ -93,11 +94,13 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
                 } else if (FIRST_NODE_EXISTS && !SECOND_NODE_EXISTS) {
                     _secondCurrentAnchor = anchor;
                     _secondCurrentAnchorNode = anchorNode;
-
+                    lineBetweenPoints(_firstCurrentAnchor, _secondCurrentAnchor);
                     SECOND_NODE_EXISTS = true;
                 }
 
                 TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+                node.getTranslationController().setEnabled(false); //makes sure you can't move the anchors by dragging
+                node.getScaleController().setEnabled(false);//makes sure you can't scale the anchors by dragging
                 node.setRenderable(cubeRenderable);
                 node.setParent(anchorNode);
                 arFragment.getArSceneView().getScene().addOnUpdateListener(this);
@@ -131,7 +134,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
                 .thenAccept(
                         material -> {
                             Vector3 vector3 = new Vector3(0.05f, 0.01f, 0.01f);
-                            cubeRenderable = ShapeFactory.makeCube(vector3, Vector3.zero(), material);
+                            cubeRenderable = ShapeFactory.makeCylinder(0.01f,0.04f, Vector3.zero(), material);
                             cubeRenderable.setShadowCaster(false);
                             cubeRenderable.setShadowReceiver(false);
                         });
@@ -157,6 +160,43 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
 
     }
 
+    public void lineBetweenPoints(Anchor _firstCurrentAnchor, Anchor _secondCurrentAnchor) {
+        AnchorNode _firstCurrentAnchorNode = new AnchorNode(_firstCurrentAnchor);
+        AnchorNode _secondCurrentAnchorNode = new AnchorNode(_secondCurrentAnchor);
+        if (_secondCurrentAnchorNode != null) {
+            _firstCurrentAnchorNode.setParent(arFragment.getArSceneView().getScene());
+            Vector3 point1, point2;
+            point1 = _secondCurrentAnchorNode.getWorldPosition();
+            point2 = _firstCurrentAnchorNode.getWorldPosition(); //eventueel niet normalized maken
+    /*
+        First, find the vector extending between the two points and define a look rotation
+        in terms of this Vector.
+    */
+            final Vector3 difference = Vector3.subtract(point1, point2);
+            final Vector3 directionFromTopToBottom = difference.normalized();
+            final Quaternion rotationFromAToB =
+                    Quaternion.lookRotation(directionFromTopToBottom, Vector3.up());
+            MaterialFactory.makeOpaqueWithColor(getApplicationContext(), new Color(0, 255, 244))
+                    .thenAccept(
+                            material -> {
+                            /* Then, create a rectangular prism, using ShapeFactory.makeCube() and use the difference vector
+                                   to extend to the necessary length.  */
+                                ModelRenderable model = ShapeFactory.makeCube(
+                                        new Vector3(.01f, .01f, difference.length()),
+                                        Vector3.zero(), material);
+                            /* Last, set the world rotation of the node to the rotation calculated earlier and set the world position to
+                                   the midpoint between the given points . */
+                                Node node = new Node();
+                                node.setParent(_firstCurrentAnchorNode);
+                                node.setRenderable(model);
+                                node.setWorldPosition(Vector3.add(point1, point2).scaled(.5f));
+                                node.setWorldRotation(rotationFromAToB);
+                            }
+                    );
+            _secondCurrentAnchorNode = _firstCurrentAnchorNode;
+        }
+    }
+
     @Override
     public void onUpdate(FrameTime frameTime) {
         Frame frame = arFragment.getArSceneView().getArFrame();
@@ -167,36 +207,36 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
             setInformationText("Select second point");
         }else if(FIRST_NODE_EXISTS && SECOND_NODE_EXISTS){
 
-                Pose firstPose = _firstCurrentAnchor.getPose();
-                Pose secondPose = _secondCurrentAnchor.getPose();
+            Pose firstPose = _firstCurrentAnchor.getPose();
+            Pose secondPose = _secondCurrentAnchor.getPose();
 
-                float dx = firstPose.tx() - secondPose.tx();
-                float dy = firstPose.ty() - secondPose.ty();
-                float dz = firstPose.tz() - secondPose.tz();
+            float dx = firstPose.tx() - secondPose.tx();
+            float dy = firstPose.ty() - secondPose.ty();
+            float dz = firstPose.tz() - secondPose.tz();
 
-                ///Compute the straight-line distance.
-                float distanceMeters = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
-                setInformationText("Difference: " + distanceMeters + " metres");
+            ///Compute the straight-line distance.
+            float distanceMeters = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+            setInformationText("Difference: " + distanceMeters + " metres");
 
-                if(DIMENTION_STATE == DimentionState.LENGTH ){
-                    _lengthWidthHeight[0] =  distanceMeters;
-                    tvLength.setText("L: " +String.valueOf(_lengthWidthHeight[0]));
-                    DIMENTION_STATE = DimentionState.NONE;
-                    tvLength.setBackgroundColor(android.graphics.Color.BLUE);
+            if(DIMENTION_STATE == DimentionState.LENGTH ){
+                _lengthWidthHeight[0] =  distanceMeters;
+                tvLength.setText("L: " +String.valueOf(_lengthWidthHeight[0]));
+                DIMENTION_STATE = DimentionState.NONE;
+                tvLength.setBackgroundColor(android.graphics.Color.BLUE);
 
-                }else if(DIMENTION_STATE == DimentionState.WIDTH  ){
-                    _lengthWidthHeight[1] =  distanceMeters;
-                    tvWidth.setText("B: " +String.valueOf(_lengthWidthHeight[1]));
-                    DIMENTION_STATE = DimentionState.NONE;
-                    tvWidth.setBackgroundColor(android.graphics.Color.BLUE);
+            }else if(DIMENTION_STATE == DimentionState.WIDTH  ){
+                _lengthWidthHeight[1] =  distanceMeters;
+                tvWidth.setText("B: " +String.valueOf(_lengthWidthHeight[1]));
+                DIMENTION_STATE = DimentionState.NONE;
+                tvWidth.setBackgroundColor(android.graphics.Color.BLUE);
 
-                }else if(DIMENTION_STATE == DimentionState.HEIGHT ) {
-                    _lengthWidthHeight[2] = distanceMeters;
-                    tvHeight.setText("H: " + String.valueOf(_lengthWidthHeight[2]));
-                    DIMENTION_STATE = DimentionState.NONE;
-                    tvHeight.setBackgroundColor(android.graphics.Color.BLUE);
+            }else if(DIMENTION_STATE == DimentionState.HEIGHT ) {
+                _lengthWidthHeight[2] = distanceMeters;
+                tvHeight.setText("H: " + String.valueOf(_lengthWidthHeight[2]));
+                DIMENTION_STATE = DimentionState.NONE;
+                tvHeight.setBackgroundColor(android.graphics.Color.BLUE);
 
-                }
+            }
 
 
         }else{
@@ -210,24 +250,24 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
 
 
     public void tappedToSetDimentionStateToLength(View v){
-            DIMENTION_STATE = DimentionState.LENGTH;
-            tvLength.setBackgroundColor(android.graphics.Color.BLUE);
+        DIMENTION_STATE = DimentionState.LENGTH;
+        tvLength.setBackgroundColor(android.graphics.Color.BLUE);
     }
 
     public void tappedToSetDimentionStateToWidth(View v){
-            DIMENTION_STATE = DimentionState.WIDTH;
-            tvWidth.setBackgroundColor(android.graphics.Color.BLUE);
+        DIMENTION_STATE = DimentionState.WIDTH;
+        tvWidth.setBackgroundColor(android.graphics.Color.BLUE);
 
     }
 
     public void tappedToSetDimentionStateToHeight(View v){
-            DIMENTION_STATE = DimentionState.HEIGHT;
-            tvHeight.setBackgroundColor(android.graphics.Color.BLUE);
+        DIMENTION_STATE = DimentionState.HEIGHT;
+        tvHeight.setBackgroundColor(android.graphics.Color.BLUE);
 
     }
 
     public void setInformationText(String text){
-            tvDistance.setText(text);
+        tvDistance.setText(text);
     }
 
 }
